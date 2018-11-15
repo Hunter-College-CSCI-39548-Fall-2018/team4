9bad43cfacf7dc8b112dbd39416b5967938e5aec
xcQuence <johnyteen@gmail.com>
2018-11-15 12:11:28 -0500

    jy-commit-test

--- database-backend/models/user.js
+++ database-backend/models/user.js
@@ -3,8 +3,7 @@ module.exports = function(sequelize, Sequelize) {
     var User = sequelize.define('Users', {
  
         id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER},
-        firstname: { type: Sequelize.STRING,notEmpty: true},
-		lastname: { type: Sequelize.STRING,notEmpty: true},
+        fullname: { type: Sequelize.STRING,notEmpty: true},
 		username: {type:Sequelize.TEXT},
 		email: { type:Sequelize.STRING, validate: {isEmail:true} },
         password : {type: Sequelize.STRING,allowNull: false }, 
--- metrotrack/src/app/services/userService.ts
+++ metrotrack/src/app/services/userService.ts
@@ -22,15 +22,15 @@ export class userService {
         }))
     }
     createUser(user: any) {
-        return this.http.post('http://localhost:5000/createUser', { params: user }, httpOptions).pipe(map((res: any) => {
+        return this.http.post('http://localhost:5000/signup', { params: user }, httpOptions).pipe(map((res: any) => {
         }))
     }
-    updateUserPreferences(user: any) {
-        return this.http.put('http://localhost:5000/updateUser', { params: user }, httpOptions).pipe(map((res: any) => {
-        }))
-    }
+    // updateUserPreferences(user: any) {
+    //     return this.http.put('http://localhost:5000/updateUser', { params: user }, httpOptions).pipe(map((res: any) => {
+    //     }))
+    // }
     authenticateUser(user: any) {
         return this.http.post('http://localhost:5000/signin', { params: user }, httpOptions).pipe(map((res: any) => {
         }))
     }
-}
+};
